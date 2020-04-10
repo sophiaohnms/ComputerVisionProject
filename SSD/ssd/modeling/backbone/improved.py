d5e68dabcd5d90e532793aa688980568b51ada26
so67 <sophia.ohnemus@gmail.com>
2020-04-10 14:35:30 +0200

    first tries

---
+++ SSD/ssd/modeling/backbone/improved.py
@@ -0,0 +1,70 @@
+import torch
+from torch import nn
+
+
+
+class ResNetModel(torch.nn.Module):
+
+    """
+    This is a basic backbone for SSD.
+    The feature extractor outputs a list of 6 feature maps, with the sizes:
+    [shape(-1, output_channels[0], 38, 38),
+     shape(-1, output_channels[1], 19, 19),
+     shape(-1, output_channels[2], 10, 10),
+     shape(-1, output_channels[3], 5, 5),
+     shape(-1, output_channels[3], 3, 3),
+     shape(-1, output_channels[4], 1, 1)]
+     where "output_channels" is the same as cfg.BACKBONE.OUT_CHANNELS
+    """
+    def __init__(self, cfg):
+        """
+            Is called when model is initialized.
+            Args:
+                image_channels. Number of color channels in image (3)
+                num_classes: Number of classes we want to predict (10)
+        """
+        super().__init__()
+        image_size = cfg.INPUT.IMAGE_SIZE
+        output_channels = cfg.MODEL.BACKBONE.OUT_CHANNELS
+        self.output_channels = output_channels
+        image_channels = cfg.MODEL.BACKBONE.INPUT_CHANNELS
+        self.output_feature_size = cfg.MODEL.PRIORS.FEATURE_MAPS
+        image_channels = 3
+
+        self.model = torchvision.models.resnet152(pretrained=True)
+
+        # remove last fully-connected layer
+        new_classifier = nn.Sequential(*list(self.model.classifier.children())[:-1])
+        model.classifier = new_classifier
+
+        # for param in self.model.parameters():  # Freeze all parameters
+        #     param.requires_grad = False
+        #
+        # for param in self.model.fc.parameters():  # Unfreeze the last fully-connected
+        #     param.requires_grad = True  # layer
+        #
+        # for param in self.model.layer4.parameters():  # Unfreeze the last 5 convolutional
+        #     param.requires_grad = True  # layers
+
+    # def forward(self, x):
+    #     x = self.model(x)
+    #     return x
+
+    def forward(self, x):
+        """
+        The forward functiom should output features with shape:
+            [shape(-1, output_channels[0], 38, 38),
+            shape(-1, output_channels[1], 19, 19),
+            shape(-1, output_channels[2], 10, 10),
+            shape(-1, output_channels[3], 5, 5),
+            shape(-1, output_channels[3], 3, 3),
+            shape(-1, output_channels[4], 1, 1)]
+        We have added assertion tests to check this, iteration through out_features,
+        where out_features[0] should have the shape:
+            shape(-1, output_channels[0], 38, 38),
+        """
+
+        x = self.model(x)
+
+        return x
+
