0301146c28d1d5e97e97d1e8776daf6a5283bb3c
so67 <sophia.ohnemus@gmail.com>
2020-04-11 09:25:39 +0200

    first tries

--- SSD/ssd/modeling/backbone/improved.py
+++ SSD/ssd/modeling/backbone/improved.py
@@ -6,32 +6,9 @@ original_model = models.resnet152(pretrained=True)
 
 class ImprovedModel(torch.nn.Module):
 
-    """
-    This is a resnet 152 backbone for SSD.
-    The feature extractor outputs a list of 6 feature maps, with the sizes:
-    [shape(-1, output_channels[0], 38, 38),
-     shape(-1, output_channels[1], 19, 19),
-     shape(-1, output_channels[2], 10, 10),
-     shape(-1, output_channels[3], 5, 5),
-     shape(-1, output_channels[3], 3, 3),
-     shape(-1, output_channels[4], 1, 1)]
-     where "output_channels" is the same as cfg.BACKBONE.OUT_CHANNELS
-    """
-
     def __init__(self, cfg):
-        """
-            Is called when model is initialized.
-            Args:
-                image_channels. Number of color channels in image (3)
-                num_classes: Number of classes we want to predict (10)
-        """
-        super(BasicModel, self).__init__()
-        image_size = cfg.INPUT.IMAGE_SIZE
-        output_channels = cfg.MODEL.BACKBONE.OUT_CHANNELS
-        self.output_channels = output_channels
-        image_channels = cfg.MODEL.BACKBONE.INPUT_CHANNELS
-        self.output_feature_size = cfg.MODEL.PRIORS.FEATURE_MAPS
-        image_channels = 3
+
+        super(ImprovedModel, self).__init__()
 
         self.features1 = nn.Sequential(
             # stop at last layer
