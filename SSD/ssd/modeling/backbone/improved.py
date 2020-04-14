315b43ea5bc6c5f154e0eebd862b7401842d8739
César Caramazana Zarzosa <100363769@alumnos.uc3m.es>
2020-04-14 16:58:59 +0200

    Feature map [3,3] for 2nd last layer

--- SSD/ssd/modeling/backbone/improved.py
+++ SSD/ssd/modeling/backbone/improved.py
@@ -10,26 +10,26 @@ class ImprovedModel(torch.nn.Module):
               
 
         super(ImprovedModel, self).__init__()
-        print(original_model)
+        #print(original_model)
         
         self.layer1 = nn.Sequential(
             *list(original_model.children())[0:6]
         )
-        #print("THIS IS LAY 1: ", self.layer1)
+        #print("Layer 1: ", self.layer1)
 
         self.layer2 = nn.Sequential(
             *list(original_model.children())[6:7]
         )
-        #print("AND THIS IS LAY 2", self.layer2)
+        #print("Layer 2: ", self.layer2)
         
         self.layer3 = nn.Sequential(
             *list(original_model.children())[7:8]
         )
-        #print("AND HERE IT COMES: ", self.layer3)
+        #print("Layer 3 ", self.layer3)
         
            
         self.layer4 = nn.Sequential(
-            nn.Conv2d(512, 256, kernel_size=(2,3), stride=1, padding=1),
+            nn.Conv2d(512, 256, kernel_size=(3,3), stride=1, padding=1),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.BatchNorm2d(256),
             nn.ReLU(),
@@ -37,20 +37,25 @@ class ImprovedModel(torch.nn.Module):
             nn.BatchNorm2d(128),
             nn.ReLU()  
         )
+        #print("Layer 4: ", self.layer4)
         
         self.layer5 = nn.Sequential(
-            nn.Conv2d(128, 128, kernel_size=(2,3), stride=1, padding=1), #From [5,4] to [2,2] or [3,3] ?
+            nn.Conv2d(128, 64, kernel_size=(1,2), stride=1, padding=1), #From [5,4] to [3,3] 
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.BatchNorm2d(128),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.Conv2d(128, 64, kernel_size=(3,3), stride=1, padding=1),
+            nn.Conv2d(64, 64, kernel_size=(3,3), stride=1, padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU()  
         )
+        
+        #print("Layer 5: ", self.layer5)
+        
         self.layer6 = nn.Sequential(
             nn.AdaptiveAvgPool2d(output_size=(1, 1))
         
         )
+        #print("Layer 6: ", self.layer6)
         
         #self.layer6 = nn.Sequential(
         #    *list(original_model.children())[8:9]
@@ -74,20 +79,6 @@ class ImprovedModel(torch.nn.Module):
         #print("L5: ", l5.size())
         l6 = self.layer6(l5)
         #print("L6: ", l6.size())
-        
-        
-        
-        #oa = self.slayer4(l6)
-        #print("finsihed: ", oa.size())
-        
-        #l4 = self.slayer4(l3)
-        #print("nowno now: ", l4.size())
-        
-        
-        #out1 = self.features1(x)
-        #out2 = self.features2(x)
-        #out3 = self.features3(x)
-        #out4 = self.features4(x)
 
         out_features = [l1, l2, l3, l4, l5, l6]
 
