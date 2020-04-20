a8e7c146e7cc1d272b4fd080508259d68c36ea5f
so67 <sophia.ohnemus@gmail.com>
2020-04-20 09:43:44 +0200

    tests

--- SSD/ssd/data/transforms/__init__.py
+++ SSD/ssd/data/transforms/__init__.py
@@ -2,7 +2,7 @@ from ssd.modeling.box_head.prior_box import PriorBox
 from .target_transform import SSDTargetTransform
 from .transforms import *
 
-normalize = transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+#normalize = transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
 
 def build_transforms(cfg, is_train=True):
     if is_train:
@@ -11,24 +11,25 @@ def build_transforms(cfg, is_train=True):
             
             #ADD SOME DATA AUGMENTATION
             RandomMirror(), #Augmentation
-            Expand(cfg.INPUT.PIXEL_MEAN),
+            #Expand(cfg.INPUT.PIXEL_MEAN),
             RandomSampleCrop(),  # "zoom-in" like in SSD paper
             ToPercentCoords(),
-            RandomBrightness(), #Basic transformations
-            RandomContrast(),
-            RandomSaturation(),
-            RandomHue(),
+            #RandomBrightness(), #Basic transformations
+            #RandomContrast(),
+            #RandomSaturation(),
+            #RandomHue(),
             # Albumentation Augmentations
-            add_weather(),
-            add_shadow(),
-            rotate(),
+            #add_weather(),
+            #add_shadow(),
+            #rotate(),
             #RandomLightingNoise(),
             #  # has to be before random sample crop
                                            # requires doubling training iterations
                                            # and is suggested by ssd paper to detect small objects
             #________________________________________
             Resize(cfg.INPUT.IMAGE_SIZE),
-            Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            SubtractMeans(cfg.INPUT.PIXEL_MEAN),
+            # Normalize([123, 117, 104], [0.229*255, 0.224*255, 0.225*255]),
             ToTensor(),
 
             # potential additional transformations
@@ -37,7 +38,8 @@ def build_transforms(cfg, is_train=True):
     else:
         transform = [
             Resize(cfg.INPUT.IMAGE_SIZE),
-            Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            SubtractMeans(cfg.INPUT.PIXEL_MEAN),
+            # Normalize([123, 117, 104], [0.229*255, 0.224*255, 0.225*255]),
             ToTensor()
         ]
     transform = Compose(transform)
