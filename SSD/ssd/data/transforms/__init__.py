241474b6a90f1d81753f8a981f60419410d02439
so67 <sophia.ohnemus@gmail.com>
2020-04-21 23:11:17 +0200

    more augmentations

--- SSD/ssd/data/transforms/__init__.py
+++ SSD/ssd/data/transforms/__init__.py
@@ -11,7 +11,7 @@ def build_transforms(cfg, is_train=True):
             
             #ADD SOME DATA AUGMENTATION
             RandomMirror(), #Augmentation
-            Expand(cfg.INPUT.PIXEL_MEAN),
+            #Expand(cfg.INPUT.PIXEL_MEAN),
             RandomSampleCrop(),  # "zoom-in" like in SSD paper
             ToPercentCoords(),
             #RandomBrightness(), #Basic transformations
